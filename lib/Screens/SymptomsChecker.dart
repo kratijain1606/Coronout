1f779ad80404755df1a77f123ceffa8bccf43403
Abhi Jain <devabhixda@gmail.com>
2020-04-16 17:00:46 +0530

    Gocorona: Add inapp webview for Symptoms checker

--- lib/Screens/SymptomsChecker.dart
+++ lib/Screens/SymptomsChecker.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 class SymptomsChecker extends StatefulWidget {
   @override
@@ -7,19 +8,19 @@ class SymptomsChecker extends StatefulWidget {
 
 class _SymptomsCheckerState extends State<SymptomsChecker> {
 
+  InAppWebViewController webView;
+  String url = "";
+  double progress = 0;
+
   @override
   void initState() {
     super.initState();
-    initialise();
-  }
-
-  void initialise() async {
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+        appBar: AppBar(
         title: Text(
           'Symptoms Checker',
           style: Theme.of(context).textTheme.title,
@@ -29,6 +30,48 @@ class _SymptomsCheckerState extends State<SymptomsChecker> {
         iconTheme: IconThemeData(color: Colors.black),
         centerTitle: true,
       ),
-   );
+        body: Container(
+          child: Column(children: <Widget>[
+            Container(
+                padding: EdgeInsets.all(10.0),
+                child: progress < 1.0
+                    ? LinearProgressIndicator(value: progress)
+                    : Container()),
+            Expanded(
+              child: Container(
+                margin: const EdgeInsets.all(10.0),
+                decoration:
+                    BoxDecoration(border: Border.all(color: Colors.blueAccent)),
+                child: InAppWebView(
+                  initialUrl: "https://covid.apollo247.com/",
+                  initialHeaders: {},
+                  initialOptions: InAppWebViewWidgetOptions(
+                      crossPlatform: InAppWebViewOptions(
+                        debuggingEnabled: true,
+                    )
+                  ),
+                  onWebViewCreated: (InAppWebViewController controller) {
+                    webView = controller;
+                  },
+                  onLoadStart: (InAppWebViewController controller, String url) {
+                    setState(() {
+                      this.url = url;
+                    });
+                  },
+                  onLoadStop: (InAppWebViewController controller, String url) async {
+                    setState(() {
+                      this.url = url;
+                    });
+                  },
+                  onProgressChanged: (InAppWebViewController controller, int progress) {
+                    setState(() {
+                      this.progress = progress / 100;
+                    });
+                  },
+                ),
+              ),
+            ),
+        ])),
+    );
   }
-} 
+}
