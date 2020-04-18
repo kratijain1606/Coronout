2ff3e123f2931ca40f5994106df1c50aa1d309ac
AjinkyaTaranekar <ajinkyataranekar@gmail.com>
2020-04-18 18:34:36 +0530

    Enhancement: New Welcome Screen

--- lib/Welcome.dart
+++ lib/Welcome.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:dots_indicator/dots_indicator.dart';
+import 'package:introduction_screen/introduction_screen.dart';
 
 import 'main.dart';
 
@@ -9,110 +9,104 @@ class Welcome extends StatefulWidget {
 }
 
 class _WelcomeState extends State<Welcome> {
-  double currentIndexPage;
-  int pageLength;
+  final introKey = GlobalKey<IntroductionScreenState>();
 
-  void initState() {
-    currentIndexPage = 0;
-    pageLength = 6;
+  void _onIntroEnd(context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (_) => MyStatefulWidget()),
+    );
+  }
+
+  Widget _buildImage(String assetName) {
+    return Align(
+      child: Image.asset('assets/images/$assetName.png', width: 350.0),
+      alignment: Alignment.bottomCenter,
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Stack(
-      children: <Widget>[
-        PageView(
-          children: <Widget>[
-            Walkthrougth(textContent: "ॐ सर्वे भवन्तु सुखिनः \nसर्वे सन्तु निरामयाः । \nसर्वे भद्राणि पश्यन्तु \nमा कश्चिद्दुःखभाग्भवेत् । \nॐ शान्तिः शान्तिः शान्तिः ॥", imageSrc: "prayinghand"),
-            Walkthrougth(textContent: "Coronout = Corona + burnout \nThe mental anguish one feels \nfrom hearing about the \nCoronavirus every minute \nof the day.", imageSrc: "doctor"),
-            Walkthrougth(textContent: "It got Symptoms Checker, \nAnd Mrs. Doc Bot \nTo help you \nStay Safe and Healthy", imageSrc: "doctors"),
-            Walkthrougth(textContent: "Also keep you Update \nwith regular news \nand stats from the world", imageSrc: "stats"),
-            Walkthrougth(textContent: "You can also donate \nCheck nearbys \nContact in crisis", imageSrc: "donate"),
-            Walkthrougth(textContent: "Get Ready to boost-up", imageSrc: "welcome"),
-          ],
-          onPageChanged: (value) {
-            setState(() => currentIndexPage = value.toDouble());
-          },
+    const bodyStyle = TextStyle(fontSize: 19.0, color: Colors.blue);
+    const pageDecoration = const PageDecoration(
+      titleTextStyle: TextStyle(fontSize: 28.0, fontWeight: FontWeight.w700, color: Colors.deepOrange),
+      bodyTextStyle: bodyStyle,
+      descriptionPadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
+      pageColor: Colors.white,
+      imagePadding: EdgeInsets.zero,
+    );
+
+    return IntroductionScreen(
+      key: introKey,
+      pages: [
+        PageViewModel(
+          title: "ॐ",
+          body:
+              "ॐ सर्वे भवन्तु सुखिनः \nसर्वे सन्तु निरामयाः । \nसर्वे भद्राणि पश्यन्तु \nमा कश्चिद्दुःखभाग्भवेत् । \nॐ शान्तिः शान्तिः शान्तिः ॥",
+          image: _buildImage('prayinghand'),
+          decoration: pageDecoration,
         ),
-        if(currentIndexPage == 5)
-          Positioned(
-          top: MediaQuery.of(context).size.height * 0.7,
-          child: Padding(
-            padding:
-                EdgeInsets.only(left: MediaQuery.of(context).size.width * 0.35),
-            child: Align(
-              alignment: Alignment.centerRight,
-              child: new RaisedButton(
-                color: Colors.blueAccent,
-                child: Text(
-                        "Enter!",
-                        style: new TextStyle(color: Colors.white),
-                      ),
-                onPressed: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => MyStatefulWidget()));  
-                },
-              ),
+        PageViewModel(
+          title: "Coronout = Corona + burnout",
+          body:
+              "The mental anguish one feels \nfrom hearing about the \nCoronavirus every minute \nof the day.",
+          image: _buildImage('doctor'),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "Features",
+          body:
+              "It got Symptoms Checker, \nAnd Mrs. Doc Bot \nTo help you \nStay Safe and Healthy",
+          image: _buildImage('doctors'),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "Updates",
+          body: "Also keep you Update \nwith regular news \nand stats from the world",
+          image: _buildImage("stats"),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "Donate",
+          body: "You can also donate \nCheck nearbys \nContact in crisis",
+          image: _buildImage("donate"),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "Welcome",
+          body: "Get Ready to boost-up",
+          image: _buildImage("welcome"),
+          footer: RaisedButton(
+            onPressed: () {
+              _onIntroEnd(context);
+            },
+            child: const Text(
+              'Enter!',
+              style: TextStyle(color: Colors.white),
+            ),
+            color: Colors.lightBlue,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8.0),
             ),
           ),
-        ),
-        Positioned(
-          top: MediaQuery.of(context).size.height * 0.9,
-          child: Padding(
-            padding:
-                EdgeInsets.only(left: MediaQuery.of(context).size.width * 0.35),
-            child: Align(
-              alignment: Alignment.centerRight,
-              child: new DotsIndicator(
-                  dotsCount: pageLength,
-                  position: currentIndexPage,
-                  decorator: DotsDecorator(
-                    color: Colors.blueAccent,
-                    activeColor: Colors.deepOrange,
-                    size: const Size.square(9.0),
-                    activeSize: const Size(18.0, 9.0),
-                    activeShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
-                  ),
-              ),
-            ),
-          ),
+          decoration: pageDecoration,
         )
       ],
-    ));
-  }
-}
-
-class Walkthrougth extends StatelessWidget {
-  final String textContent;
-  final String imageSrc;
-  Walkthrougth({Key key, @required this.textContent, @required this.imageSrc }) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(color: Colors.white70),
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height,
-      child: Column(
-        children: <Widget>[
-          SizedBox(height: MediaQuery.of(context).size.height * 0.2,),
-          Image(
-            image: AssetImage("assets/images/$imageSrc.png"),
-          ),
-          Row(
-            children: <Widget>[
-              SizedBox(width: MediaQuery.of(context).size.width * 0.12,),
-              Text(
-                textContent,
-                style: new TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20.0,
-                  color: Colors.blueAccent),
-              ),
-              SizedBox(width: MediaQuery.of(context).size.width * 0.1,),
-            ],
-          ),    
-        ],
-      )
+      onDone: () => _onIntroEnd(context),
+      //onSkip: () => _onIntroEnd(context), // You can override onSkip callback
+      showSkipButton: true,
+      skipFlex: 0,
+      nextFlex: 0,
+      skip: const Text('Skip'),
+      next: const Icon(Icons.arrow_forward),
+      done: const Text('Done', style: TextStyle(fontWeight: FontWeight.w600)),
+      dotsDecorator: const DotsDecorator(
+        size: Size(10.0, 10.0),
+        color: Color(0xFFBDBDBD),
+        activeSize: Size(22.0, 10.0),
+        activeShape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(25.0)),
+        ),
+      ),
     );
   }
 }
